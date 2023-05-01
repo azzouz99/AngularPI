ba9c7d2b33d41f9eaea111544045a0df3057118e
HAZEM CHTIOUI <hazem.chtioui@esprit.tn>
2023-05-01 20:06:26 +0200

    front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {FrontHomeComponent} from "./front-home/front-home.component";
+import {OfferComponent} from "./offer/offer.component";
+import {TeamsComponent} from "./teams/teams.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"navbar",component:NavbarComponent},
+  {path:"front-home",component:FrontHomeComponent},
+  {path:"offer",component:OfferComponent},
+  {path:"teams",component:TeamsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FrontHomeComponent } from './front-home/front-home.component';
+import { OfferComponent } from './offer/offer.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FrontHomeComponent,
+    OfferComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
